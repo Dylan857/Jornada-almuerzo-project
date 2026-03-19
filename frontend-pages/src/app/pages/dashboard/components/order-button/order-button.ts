eaff18cd9592e4531f9c7f278719f8a9a6cc2284
Dylan Vasquez <dylanvazquez0904@gmail.com>
2026-03-19 13:24:56 -0500

    Add validation in input

--- frontend-pages/src/app/pages/dashboard/components/order-button/order-button.ts
+++ frontend-pages/src/app/pages/dashboard/components/order-button/order-button.ts
@@ -10,40 +10,40 @@ import { OrderService } from '../../../../core/services/order.service';
 export class OrderButton {
   private orderService = inject(OrderService);
 
+  quantityValue = 1;
   quantity = signal(1);
   loading = signal(false);
   lastOrderId = signal<string | null>(null);
   error = signal<string | null>(null);
   errorValue = signal(false);
 
-  onQuantityChange(value: string) {
-    if (value === '') {
+  onQuantityChange(value: number) {
+    if (!value || value <= 0) {
+      this.quantityValue = 1;
       this.quantity.set(1);
-      return;
+    } else {
+      this.quantity.set(value);
     }
-
-    const num = parseInt(value);
-    if (num > 0) this.quantity.set(num);
+    this.errorValue.set(false);
   }
 
   placeOrder() {
     if (this.loading()) return;
+
+    if (this.quantity() <= 0) {
+      this.errorValue.set(true);
+      return;
+    }
+
     this.loading.set(true);
     this.error.set(null);
     this.lastOrderId.set(null);
-
-    if (this.quantity() <= 0 || this.quantity() === null) {
-      this.errorValue.set(true);
-      this.loading.set(false);
-      return;
-    }
 
     this.orderService.createOrder(this.quantity()).subscribe({
       next: (res) => {
         this.lastOrderId.set(res.data.orderId);
         this.loading.set(false);
         this.orderService.loadOrders();
-        this.quantity.set(1);
       },
       error: () => {
         this.error.set('Error al crear el pedido. Intenta de nuevo.');
